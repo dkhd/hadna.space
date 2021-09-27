d7028c42f9bd3a6b76e6ecb027eb66e21707a466
Diky Hadna <dikyhd@gmail.com>
2021-09-27 22:07:42 +0700

    Update: Contribution to OSS

--- src/components/Home/OSSContribution.jsx
+++ src/components/Home/OSSContribution.jsx
@@ -3,17 +3,17 @@ import * as React from "react";
 const LatestPost = () => {
   return (
     <div className="flex flex-col justify-center py-20">
-      <div className="px-16 mx-auto">
+      <div className="px-5 md:px-16 mx-auto">
         <p className="text-5xl text-gray-600 font-title font-sans font-bold text-center tracking-wide">
           Contribution to OSS
         </p>
         <p className="text-2xl text-gray-700 font-light text-gray-700 text-center mt-3 md:w-6/12 mx-auto mt-10">
-          I have a special interest in OSS (Open Source Software). And while
+          My first introduction to OSS (Open Source Software) was in 2007 when I was a high school student. Since then, I developed a special interest in using OSS and it helped me in so many ways to become who I am today. Now, I want to return the favor by contributing to OSS. And while
           it's not that much of a contribution, I like to spend some of my time
           to contribute to OSS.
         </p>
         <p className="text-2xl text-gray-700 font-light text-gray-700 text-center mt-3 md:w-6/12 mx-auto mt-10">
-          Here are some of my contributions to OSS (as a contributor):
+          Here are some of my small contributions to OSS:
           <ol className="mt-1">
             <li>
               <a
@@ -47,9 +47,7 @@ const LatestPost = () => {
           >
             Github profile
           </a>{" "}
-          and drop stars to my public repositories. You can also recommend me to
-          contribute to an OSS project (no matter how small the project is, I'll
-          do my best). In the meanwhile, these are my Github stats generated
+          and drop stars to my public repositories. You can also help me to expand the list above by giving me a suggestion to an OSS project you already know or use (or if you are a maintainer of an OSS project and need help, ping me!). <br/><br/>In the meanwhile, these are my Github stats generated
           from{" "}
           <a
             href="https://rahuldkjain.github.io/gh-profile-readme-generator/"
@@ -58,31 +56,31 @@ const LatestPost = () => {
             className="text-dkhd-purple-500 border-b-2 border-dashed border-dkhd-purple hover:text-dkhd-purple-600 font-semibold"
           >
             Github Profile Generator
-          </a>
+          </a>.
         </p>
-        <div className="flex flex-col gap-5 px-10 mx-auto w-full mt-10">
+        <div className="flex flex-col gap-5 md:px-10 mx-auto w-full mt-10">
           <img
             src="https://github-profile-trophy.vercel.app/?username=dkhd"
             alt="dkhd"
-            className="mx-auto w-10/12 md:w-6/12"
+            className="mx-auto w-full md:w-6/12"
           />
           <img
             align="center"
             src="https://github-readme-stats.vercel.app/api/top-langs?username=dkhd&show_icons=true&locale=en&layout=compact"
             alt="dkhd"
-            className="mx-auto w-10/12 md:w-4/12"
+            className="mx-auto w-full md:w-4/12"
           />
           <img
             align="center"
             src="https://github-readme-stats.vercel.app/api?username=dkhd&show_icons=true&locale=en"
             alt="dkhd"
-            className="mx-auto w-10/12 md:w-4/12"
+            className="mx-auto w-full md:w-4/12"
           />
           <img
             align="center"
             src="https://github-readme-streak-stats.herokuapp.com/?user=dkhd&"
             alt="dkhd"
-            className="mx-auto w-10/12 md:w-4/12"
+            className="mx-auto w-full md:w-4/12"
           />
         </div>
       </div>
